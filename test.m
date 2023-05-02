ff9f9a937eae767dfaf271b486c57bf870b3749d
renjiaren <ren17865780629@163.com>
2023-05-02 23:18:26 +0800

    5.2

--- test.m
+++ test.m
@@ -1,16 +1,26 @@
 clc;
 clear
-import ModelRegenerate
-import GPUCalculate
 
-creobridgePath = 'E:\SRM\SRM\creobridge5';
+% 必填项
+SRMPath = 'E:\SRM\SRM';
 tempFolderPath = 'D:\Temp';
 modelPath = 'D:\Model';
 modelName = 'Grain';
-IBSPath = 'E:\SRM\SRM\IBS';
+
+creobridgePath = [SRMPath, '\creobridge5'];
+IBSPath = [SRMPath, '\IBS'];
+ModelRegeneratePath = [SRMPath, '\ModelRegenerate'];
+GPUCalculatePath = [SRMPath, '\GPUCalculate'];
+ooDACEPath = [SRMPath, '\ooDACE'];
+
+addpath(genpath(ModelRegeneratePath));
+addpath(genpath(GPUCalculatePath));
+addpath(genpath(ooDACEPath));
+
 pauseTime = 1;
 parameters = ["L_1", "L_2", "Alpha_1", "Beta_1"];
 value = [200, 300, 60.52, 70.98];
+
 m = ModelRegenerate(creobridgePath, tempFolderPath, modelPath, modelName, IBSPath, pauseTime, parameters, value);
 m.execute();
 
